2d8491e216220248c0f162df15960b8565e5e0de
Brand07 <81128304+Brand07@users.noreply.github.com>
2025-03-15 19:47:54 -0500

    Double vs float

--- app/src/main/java/com/example/kotlinbasics/Basics.kt
+++ app/src/main/java/com/example/kotlinbasics/Basics.kt
@@ -1,10 +1,7 @@
 package com.example.kotlinbasics
 
 fun main(){
-    //Val cannot be changed
-    val number_one = 1
-    //Var can be changed
-    var myAge: Byte = 28
-    myAge = 29
-    println("I am $myAge years old.")
+    //Double can be changed to float by putting a "f" at the end of the double
+    val pi = 3.14f
+    println(pi)
 }
