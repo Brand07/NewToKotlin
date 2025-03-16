41819e2dfed0a19abe126d2b42ff8bef0fdbe7e1
Brand07 <81128304+Brand07@users.noreply.github.com>
2025-03-15 19:52:00 -0500

    Unsigned integers

--- app/src/main/java/com/example/kotlinbasics/Basics.kt
+++ app/src/main/java/com/example/kotlinbasics/Basics.kt
@@ -1,7 +1,7 @@
 package com.example.kotlinbasics
 
 fun main(){
-    //Double can be changed to float by putting a "f" at the end of the double
-    val pi = 3.14f
-    println(pi)
+
+    var age: UShort = 29u
+    println(age)
 }
