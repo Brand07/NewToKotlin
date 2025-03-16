7a411a0facaebc4c6856d4996451518bb5d4d687
Brand07 <81128304+Brand07@users.noreply.github.com>
2025-03-15 19:58:00 -0500

    Add boolean examples

---
+++ app/src/main/java/com/example/kotlinbasics/Booleans.kt
@@ -0,0 +1,16 @@
+package com.example.kotlinbasics
+
+// Logic Operations
+
+// || OR
+// && AND
+// | NOT
+
+fun main(){
+    val myTrue: Boolean = true
+    val myFalse: Boolean = false
+
+    println(myTrue || myFalse)
+    println(myTrue && myFalse)
+    println(!myTrue)
+}
