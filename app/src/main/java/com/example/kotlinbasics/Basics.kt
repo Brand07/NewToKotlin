520994fc64542944e9a5c298349d9f668e1eddd8
Brand07 <81128304+Brand07@users.noreply.github.com>
2025-03-15 19:31:34 -0500

    val vs var

--- app/src/main/java/com/example/kotlinbasics/Basics.kt
+++ app/src/main/java/com/example/kotlinbasics/Basics.kt
@@ -1,5 +1,10 @@
 package com.example.kotlinbasics
 
 fun main(){
+    //Val cannot be changed
+    val number_one = 1
+    //Var can be changed
+    var myAge = 29
+    myAge = 30
     println("Hello World!")
 }
