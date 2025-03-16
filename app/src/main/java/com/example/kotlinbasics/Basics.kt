41f68f464ada3f73c7ac16af274a59cd4b2be6be
Brand07 <81128304+Brand07@users.noreply.github.com>
2025-03-15 20:00:52 -0500

    Init example

--- app/src/main/java/com/example/kotlinbasics/Basics.kt
+++ app/src/main/java/com/example/kotlinbasics/Basics.kt
@@ -1,7 +1,7 @@
 package com.example.kotlinbasics
 
 fun main(){
-
-    var age: UShort = 29u
-    println(age)
+    //Double can be changed to float by putting a "f" at the end of the double
+    val pi = 3.14f
+    println(pi)
 }
