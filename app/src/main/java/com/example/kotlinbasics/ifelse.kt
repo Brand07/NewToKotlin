da9198e00b2e43c59fe1238caa9af3d3c6dcc227
Brand07 <81128304+Brand07@users.noreply.github.com>
2025-03-16 22:34:13 -0500

    example of if/else

---
+++ app/src/main/java/com/example/kotlinbasics/ifelse.kt
@@ -0,0 +1,14 @@
+package com.example.kotlinbasics
+
+fun main(){
+
+    var age = 29
+
+    if(age >= 21){
+        //Execute this
+        println("Old enough")
+    }else{
+        //if not true, execute this
+        println("Not old enough")
+    }
+}
