d1282bbf89dc6b60f678ecef3f264f045ff6854e
Brand07 <81128304+Brand07@users.noreply.github.com>
2025-03-17 18:41:21 -0500

    Learn "when" statements

--- RPS/app/src/main/java/com/example/rockpaperscissors/RockPaperScissors.kt
+++ RPS/app/src/main/java/com/example/rockpaperscissors/RockPaperScissors.kt
@@ -1,5 +1,33 @@
 package com.example.rockpaperscissors
 
 fun main(){
+    var computerChoice = ""
+    var playerChoice = ""
+    println("Rock, Paper, Scissors? Enter your choice: ")
+    playerChoice = readln()
+
+    val randomNumber = (1..3).random()
+
+    when (randomNumber) {
+        1 -> {
+            computerChoice = "Rock"
+        }
+        2 -> {
+            computerChoice = "Paper"
+        }
+        else -> {
+            computerChoice = "Scissors"
+        }
+    }
+    println(computerChoice)
+
+    //Store the winner
+    val winner = when {
+        playerChoice == computerChoice -> "Tie"
+        playerChoice == "Rock" && computerChoice == "Scissors" -> "Player"
+        playerChoice == "Paper" && computerChoice == "Rock" -> "Player"
+        playerChoice == "Scissors" && computerChoice == "Paper" -> "Player"
+        else -> "Computer"
+    }
 
 }
